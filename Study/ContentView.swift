d7b4f7693a47a2046c5cb62bd674baa062cf369e
kazua <baitianzong@gmail.com>
2021-10-04 14:23:15 +0900

    Initial Commit

---
+++ Study/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Study
+//
+//  Created by cmStudent on 2021/10/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
