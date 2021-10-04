e293c3e835371998c21759f5ad0249760b6657b4
purin8840 <bboy_purin127@yahoo.co.jp>
2021-10-04 18:30:28 +0900

    test

--- Study/ContentView.swift
+++ Study/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hellowoorld!")
+        Text("Hellowoooooorld!")
             .padding()
         // pad
         //di   ng
