18a709198a1a9e30c13542248e927d2b2769386e
kazua4396 <88267594+kazua4396@users.noreply.github.com>
2021-10-04 16:09:44 +0900

    Update ContentView.swift

--- Study/ContentView.swift
+++ Study/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Helloworld!")
+        Text("Hellowooooooooooooorld!")
             .padding()
         // pad
         //di   ng
