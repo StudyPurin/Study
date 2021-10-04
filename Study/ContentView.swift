d51a6bd2b9f03b195e0a29e207458a525eac566d
hide <jyuni21@icloud.com>
2021-10-04 18:48:02 +0900

    hello

--- Study/ContentView.swift
+++ Study/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
 
-        Text("Hellowooo   oooooooorld!")
+        Text("Hellowooooooooorld!")
 
         Text("Hellowoooooorld!")
             .padding()
