c7dd41f3d7b7945825b14789835de9024d85a6ef
kazua <baitianzong@gmail.com>
2021-10-11 13:54:36 +0900

    test

--- Study/SideMenuContentView.swift
+++ Study/SideMenuContentView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct SideMenuView: View {
     @Binding var isOpen: Bool
-    @Binding var num: Int
     let width: CGFloat = 270
     
     var body: some View {
@@ -27,9 +26,9 @@ struct SideMenuView: View {
             
             HStack {
                 VStack() {
-                    SideMenuContentView(topPadding: 100, systemName: "books.vertical", text: "統計", isOpen: $isOpen, num: 0)
-                    SideMenuContentView(systemName: "pencil", text: "記録",  isOpen: $isOpen, num: 1)
-                    SideMenuContentView(systemName: "timer", text: "タイマー", isOpen: $isOpen, num: 2)
+                    SideMenuContentView(topPadding: 100, systemName: "books.vertical", text: "統計", isOpen: $isOpen)
+                    SideMenuContentView(systemName: "pencil", text: "記録",  isOpen: $isOpen)
+                    SideMenuContentView(systemName: "timer", text: "タイマー", isOpen: $isOpen)
                     Spacer()
                 }
                 .frame(width: width)
@@ -47,37 +46,34 @@ struct SideMenuContentView: View {
     let topPadding: CGFloat
     let systemName: String
     let text: String
-    let num: Int
     @Binding var isOpen: Bool
+    @State var num = 0;
     
-    init(topPadding: CGFloat = 30, systemName: String, text: String, isOpen: Binding<Bool>, num: Int ) {
+    init(topPadding: CGFloat = 30, systemName: String, text: String, isOpen: Binding<Bool>) {
         self.topPadding = topPadding
         self.systemName = systemName
         self._isOpen = isOpen
         self.text = text
-        self.num = num
     }
     
     var body: some View {
-        if (num == 0) {
-            HStack {
-                Image(systemName: systemName)
-                    .foregroundColor(.gray)
-                    .imageScale(.large)
-                    .frame(width: 32.0)
-                Text(text)
-                    .foregroundColor(.gray)
-                    .font(.headline)
-                Spacer()
-            }
-            .padding(.top, topPadding)
-            .padding(.leading, 32)
-            .onTapGesture {
-                ContentView()
-                self.isOpen = false
-            }
+        HStack {
+            Image(systemName: systemName)
+                .foregroundColor(.gray)
+                .imageScale(.large)
+                .frame(width: 32.0)
+            Text(text)
+                .foregroundColor(.gray)
+                .font(.headline)
+            Spacer()
+        }
+        .padding(.top, topPadding)
+        .padding(.leading, 32)
+        .onTapGesture {
+            self.isOpen = false
         }
     }
 }
+
 
 
